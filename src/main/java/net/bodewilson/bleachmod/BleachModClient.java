074601562f0c60bf958ed57fa03d8bed1e687215
Bodewilson <corvus.dead01@gmail.com>
2022-07-04 14:14:33 -0300

    Initial commit Empty Mod

---
+++ src/main/java/net/bodewilson/bleachmod/BleachModClient.java
@@ -0,0 +1,12 @@
+package net.bodewilson.bleachmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BleachModClient implements ClientModInitializer
+{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
