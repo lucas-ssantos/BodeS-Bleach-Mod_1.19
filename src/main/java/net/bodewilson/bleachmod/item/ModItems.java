170b52b91ec037a76b3aaf7533eeb5d68a420868
Bodewilson <corvus.dead01@gmail.com>
2022-07-07 01:07:16 -0300

    Added -Arrancar armor textures -Captain Shinigami textures -Shinigami armor (need better animations)

--- src/main/java/net/bodewilson/bleachmod/item/ModItems.java
+++ src/main/java/net/bodewilson/bleachmod/item/ModItems.java
@@ -1,7 +1,11 @@
 package net.bodewilson.bleachmod.item;
 
 import net.bodewilson.bleachmod.BleachMod;
+import net.bodewilson.bleachmod.item.custom.ModArmorItem;
+import net.bodewilson.bleachmod.item.custom.ShihakushoArmorItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.SwordItem;
 import net.minecraft.util.Identifier;
@@ -11,12 +15,21 @@ public class ModItems
 {
     public static final Item REIRYOKU = registerItem("reiryoku",
             new Item(new FabricItemSettings().group(ModItemGroup.BLEACHMOD)));
-
     public static final Item REIATSU_INGOT = registerItem("reiatsu_ingot",
             new Item(new FabricItemSettings().group(ModItemGroup.BLEACHMOD)));
 
     public static final Item ZANPAKTO = registerItem("zanpakto",
             new SwordItem(ModToolMaterials.REIATSU, 6, 2F,
+                    new FabricItemSettings().group(ModItemGroup.BLEACHMOD)));
+
+    public static final Item SHIHAKUSHO_CHESTPLATE = registerItem("shihakusho_chestplate",
+            new ShihakushoArmorItem(ModArmorMaterials.REIATSU, EquipmentSlot.CHEST,
+                    new FabricItemSettings().group(ModItemGroup.BLEACHMOD)));
+    public static final Item SHIHAKUSHO_LEGGINGS = registerItem("shihakusho_leggings",
+            new ShihakushoArmorItem(ModArmorMaterials.REIATSU, EquipmentSlot.LEGS,
+                    new FabricItemSettings().group(ModItemGroup.BLEACHMOD)));
+    public static final Item SANDALS = registerItem("sandals",
+            new ShihakushoArmorItem(ModArmorMaterials.REIATSU, EquipmentSlot.FEET,
                     new FabricItemSettings().group(ModItemGroup.BLEACHMOD)));
 
 
